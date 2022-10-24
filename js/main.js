ffdb1f8b2ec450e23dcc895304c80fb4fd2f6967
maurycywoznica <mjw11@wp.pl>
2022-10-24 22:31:25 +0200

    :(

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const burgerMenu = document.getElementsByClassName('burger-btn')
-burgerMenu.style.display= 'block';
-
-const cardRoll = (e, c) => {
-    e.style.
-}
-
-addEventListener('click', cardRoll())
