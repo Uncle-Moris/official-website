bb2dcfd6b5d66e41f5351913ee91a836f838f756
maurycywoznica <mjw11@wp.pl>
2022-11-30 21:05:56 +0100

    new structure

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+const footerYear = document.querySelector('.footer__year')
+
+
+const year = new Date().getFullYear();
+
+footerYear.append(`${year}`)
+
+
+// nav
+const burgerBtn = document.querySelector('.burger-btn')
+
+burgerBtn.addEventListener('click', ()=> {})
