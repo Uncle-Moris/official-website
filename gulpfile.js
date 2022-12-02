38e3f8c97efb7ac07eb83699cd51f99aaf809d4a
maurycywoznica <mjw11@wp.pl>
2022-12-02 21:02:31 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
-const {src, dest} = require('gulp');
+const {src, dest, series} = require('gulp');
 
 const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
 
 function sassCompiler(done) {
     src('./src/sass/**/*.scss')
 
         .pipe(sass().on('error', sass.logError))
+        .pipe(cssnano())
         .pipe(dest('./dist/css'));
     done()
 }
 
 
 
-exports.default = sassCompiler
+exports.default = series(sassCompiler, )
