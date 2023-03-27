7c795152f5fce525e9ba62f582d2dd2024f2c614
maurycywoznica <mjw11@wp.pl>
2023-03-27 23:52:19 +0200

    react app innit

---
+++ mysite/src/components/navigation.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Navigation extends Component {
+    render() {
+        return (
+        <p>Hello Goofs</p>
+        )}
+}
+
+
+
+export default Navigation
