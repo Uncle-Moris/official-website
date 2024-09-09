5eec5645452061218010e97215f651bbb6eda5d4
Uncle_Moris <mjw11@wp.pl>
2024-09-10 00:56:04 +0200

    new index.html site

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import maurycyImage from './maurycy.jpg'; // Import Twojego zdjęcia
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, it's Maurycy here!</h1>
+        <p>Your favorite DevOps</p> {/* Zmieniony tekst */}
+        <img src={maurycyImage} className="App-logo" alt="Maurycy" />
+        <p className="bottom-text">
+          The sole purpose of this page is to set up CI/CD for index.html, and that's it.
+        </p> {/* Mały tekst przypięty do dołu nagłówka */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
