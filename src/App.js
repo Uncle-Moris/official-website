4ca801f1565818f4f116a0138fb7c72e215725bf
Uncle_Moris <mjw11@wp.pl>
2024-09-10 01:35:32 +0200

    add CI/CD

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import maurycyImage from './maurycy.jpg'; // Import Twojego zdjęcia
+import maurycyImage from './maurycy.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello, it's Maurycy here!</h1>
-        <p>Your favorite DevOps</p> {/* Zmieniony tekst */}
+        <p>Your favorite DevOps</p>
         <img src={maurycyImage} className="App-logo" alt="Maurycy" />
         <p className="bottom-text">
-          The sole purpose of this page is to set up CI/CD for index.html, use AWS , and that's it.
-        </p> {/* Mały tekst przypięty do dołu nagłówka */}
+          The sole purpose of this page is to set up CI/CD for index.html, use AWS , and that's it XD.
+        </p>
       </header>
     </div>
   );
